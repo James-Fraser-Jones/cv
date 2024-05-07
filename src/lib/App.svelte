d02151da1869ab35cf70f19d6cddaed1f72e8fb2
James Fraser-Jones <james@fraser-jones.co.uk>
2024-05-07 13:23:19 +0100

    added remaining icons, theme color, helvetica default font

--- src/lib/App.svelte
+++ src/lib/App.svelte
@@ -1,9 +1,22 @@
 <script>
 	import Fa from 'svelte-fa'
-  	import { faFlag } from '@fortawesome/free-solid-svg-icons'
-	let name = 'world';
+  	import { faSuitcase, faTasks, faMortarBoard, faLaptop, faMobilePhone, faEnvelope, faInfo, faMapMarker, faLink, faAngleRight, faGlobe } from '@fortawesome/free-solid-svg-icons'
+	import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+	let name = 'James Fraser-Jones';
 </script>
 
-<h1 class="text-3xl font-bold underline">Hello {name}!</h1>
+<h1 class="text-brand">Hello {name}!</h1>
 
-<Fa icon={faFlag}/>Flag
+<Fa icon={faSuitcase}/>
+<Fa icon={faTasks}/>
+<Fa icon={faMortarBoard}/>
+<Fa icon={faLaptop}/>
+<Fa icon={faMobilePhone}/>
+<Fa icon={faEnvelope}/>
+<Fa icon={faInfo}/>
+<Fa icon={faMapMarker}/>
+<Fa icon={faLink}/>
+<Fa icon={faAngleRight}/>
+<Fa icon={faGlobe}/>
+<Fa icon={faLinkedin}/>
+<Fa icon={faGithub}/>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: '#0e8c7f',
+      }
+    },
+    fontFamily: {
+      'sans': ['"Inter Tight"', ...defaultTheme.fontFamily.sans],
+    }
   },
   plugins: [],
 }
