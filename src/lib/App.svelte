6bfdbe0adad32a3f2f35c1f61ca1c9abcad2d327
James Fraser-Jones <james@fraser-jones.co.uk>
2024-05-07 03:41:10 +0100

    added fontawesome

--- src/lib/App.svelte
+++ src/lib/App.svelte
@@ -1,5 +1,9 @@
 <script>
+	import Fa from 'svelte-fa'
+  	import { faFlag } from '@fortawesome/free-solid-svg-icons'
 	let name = 'world';
 </script>
 
 <h1 class="text-3xl font-bold underline">Hello {name}!</h1>
+
+<Fa icon={faFlag}/>Flag
