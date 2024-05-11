fba6f63c539e2082e0921818e7ab0b450e0daad2
James Fraser-Jones <james@fraser-jones.co.uk>
2024-05-11 04:29:53 +0100

    added proper sun and moon icons

--- src/lib/CV.svelte
+++ src/lib/CV.svelte
@@ -11,7 +11,7 @@
 <div class="flex justify-center p-14 bg-slate-200 overflow-hidden min-h-screen">
 	<div
 		class="
-			flex flex-col gap-3 p-14 bg-slate-100 shadow-2xl
+			flex flex-col gap-3 p-14 bg-slate-50 shadow-2xl
 			w-a4 h-a4
 			leading-tight tracking-normal
 			relative
@@ -33,4 +33,6 @@
 		pointer-events-none
 		backdrop-invert backdrop-hue-rotate-180
 	"
-/>
+>
+	<img src="sun.svg" alt="triangle with all three sides equal" height="600" width="600" />
+</div>
